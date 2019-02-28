104f4796471612070109a6788155ebc61703c0b0
Isaac Hall and Joe Pritchett <isaac@>
2019-02-27 16:45:22 -0800

    Finish Sketch Desiesigsign

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,60 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry:'./src/index.js',
+
+  entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+
   devtool: 'eval-source-map',
   devServer: {
-    contentBase: '.dist'
+    contentBase: './dist'
   },
+
   module: {
     rules: [
+
       {
         test: /\.scss$/,
         use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
+            "style-loader",
+            "css-loader",
+            "sass-loader"
         ]
-      }
+      },
+
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+
     ]
   },
+
   plugins: [
     new HtmlWebpackPlugin({
       inject: 'body',
       template: './src/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
     })
+
   ]
+
 };
